6d0ccbc58d5aa697af1601b795930081636751aa
70985_Sudhanshu <sudhanshukulkarni98@gmail.com>
2024-03-22 15:03:39 +0530

    obj of RestaurantRepo in service for buisness logic and created restaurantmapper interface which maps it to service of DTOs

---
+++ src/main/java/com/example/demo/controller/RestaurantController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.RestaurantDTO;
+import com.example.demo.service.RestaurantService;
+
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+	
+	@Autowired
+	RestaurantService restaurantService;
+	
+	@GetMapping("/fetchAllRestaurants")
+	public  ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
+		List<RestaurantDTO> allRestaurant = restaurantService.findAllRestaurants();
+		return new ResponseEntity<>(allRestaurant,HttpStatus.OK);
+	}
+	
+
+}
--- src/main/java/com/example/demo/service/RestaurantService.java
+++ src/main/java/com/example/demo/service/RestaurantService.java
@@ -1,5 +1,26 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.RestaurantDTO;
+import com.example.demo.entity.Restaurant;
+import com.example.demo.repo.RestaurantRepo;
+
+@Service
 public class RestaurantService {
+	
+	@Autowired
+	RestaurantRepo restaurantrepo;
+
+	public List<RestaurantDTO> findAllRestaurants() {
+		List<Restaurant> restaurants = restaurantrepo.findAll();
+		//map it to list of DTO's
+		
+		
+		
+	}
 
 }
