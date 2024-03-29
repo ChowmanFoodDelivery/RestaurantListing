b8f27607ad8b0e094d82bf5ec703ced32c15d1cb
70985_Sudhanshu <sudhanshukulkarni98@gmail.com>
2024-03-29 11:48:29 +0530

    added fetch by id

--- src/main/java/com/example/demo/controller/RestaurantController.java
+++ src/main/java/com/example/demo/controller/RestaurantController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.RestaurantDTO;
+import com.example.demo.entity.Restaurant;
 import com.example.demo.service.RestaurantService;
 
 @RestController
@@ -33,6 +35,10 @@ public class RestaurantController {
 		return new ResponseEntity<>(restaurantAdded,HttpStatus.CREATED);
 	}
 	
+	@GetMapping("/fetchById/{id}")
+	public ResponseEntity<RestaurantDTO> findRestaurantById(@PathVariable Integer id){
+		return restaurantService.fetchRestaurantById(id);
+	}
 	
 	
 
--- src/main/java/com/example/demo/service/RestaurantService.java
+++ src/main/java/com/example/demo/service/RestaurantService.java
@@ -1,9 +1,13 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.RestaurantDTO;
@@ -11,26 +15,34 @@ import com.example.demo.entity.Restaurant;
 import com.example.demo.mapper.RestaurantMapper;
 import com.example.demo.repo.RestaurantRepo;
 
+
 @Service
 public class RestaurantService {
-	
+
 	@Autowired
 	RestaurantRepo restaurantrepo;
-
+       
 	public List<RestaurantDTO> findAllRestaurants() {
 		List<Restaurant> restaurants = restaurantrepo.findAll();
 		//map it to list of DTO's
-		
 		List<RestaurantDTO> restaurantDTOList =  restaurants.stream().map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant)).collect(Collectors.toList());
 		return restaurantDTOList;
-		
-		
 	}
 
-	public RestaurantDTO addRestaurantInDB(RestaurantDTO restaurantDTO) {
+public RestaurantDTO addRestaurantInDB(RestaurantDTO restaurantDTO) {
 		
 		Restaurant savedRestaurant = restaurantrepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
 			return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(savedRestaurant);
 	}
+
+public ResponseEntity<RestaurantDTO> fetchRestaurantById(Integer id) {
+	
+			Optional<Restaurant> restaurant = restaurantrepo.findById(id);
+			if(restaurant.isPresent()) {
+				return new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant.get()), HttpStatus.OK);
+			}
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			
+}
 
 }
